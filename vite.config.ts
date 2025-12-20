73107db1e644b0db41a9c0c5647fadf8fa22b23e
Ivan <77185900+wpinrui@users.noreply.github.com>
2025-12-20 10:24:20 +0800

    Fix vite config import order

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { readFileSync } from 'fs'
+import { defineConfig } from 'vite'
 
 const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
