ba1c0fe71dd4dc7487ff0a6e9ef5a37bc246dcd9
Ivan <77185900+wpinrui@users.noreply.github.com>
2025-12-25 12:01:32 +0800

    Sort upgrading courses by course code

--- src/data/upgrading-courses/index.ts
+++ src/data/upgrading-courses/index.ts
@@ -25,19 +25,15 @@
 
 import type { UpgradingCourse } from '../../types';
 
-// Biology
+// QUB - Biology
 import QUB511 from './QUB511.json';
 import QUB513 from './QUB513.json';
 
-// Chemistry
-import QUY511 from './QUY511.json';
-import QUY512 from './QUY512.json';
-
-// Chinese Language
+// QUC - Chinese Language
 import QUC501 from './QUC501.json';
 import QUC509 from './QUC509.json';
 
-// English Language
+// QUE - English Language
 import QUE512_G1 from './QUE512-G1.json';
 import QUE512_G2 from './QUE512-G2.json';
 import QUE512_G3 from './QUE512-G3.json';
@@ -49,47 +45,48 @@ import QUE513_G3 from './QUE513-G3.json';
 import QUE513_G4 from './QUE513-G4.json';
 import QUE513_G5 from './QUE513-G5.json';
 
-// English Literature
-import QUR511 from './QUR511.json';
-
-// Geography
+// QUG - Geography
 import QUG511 from './QUG511.json';
 import QUG512 from './QUG512.json';
 
-// History
+// QUH - History
 import QUH512 from './QUH512.json';
 import QUH513 from './QUH513.json';
 
-// Physics
+// QUP - Physics
 import QUP511 from './QUP511.json';
 import QUP512 from './QUP512.json';
 
-// Science
+// QUR - English Literature
+import QUR511 from './QUR511.json';
+
+// QUS - Science
 import QUS511_G1 from './QUS511-G1.json';
 import QUS511_G2 from './QUS511-G2.json';
 import QUS511_G3 from './QUS511-G3.json';
 import QUS511_G4 from './QUS511-G4.json';
 
-// Tamil Language
+// QUT - Tamil Language
 import QUT501 from './QUT501.json';
 import QUT502 from './QUT502.json';
 import QUT503 from './QUT503.json';
+
+// QUY - Chemistry
+import QUY511 from './QUY511.json';
+import QUY512 from './QUY512.json';
 
 /**
  * All available upgrading courses.
  * Add new courses here after importing their JSON files.
  */
 export const UPGRADING_COURSES: UpgradingCourse[] = [
-  // Biology
+  // QUB - Biology
   QUB511,
   QUB513,
-  // Chemistry
-  QUY511,
-  QUY512,
-  // Chinese Language
+  // QUC - Chinese Language
   QUC501,
   QUC509,
-  // English Language
+  // QUE - English Language
   QUE512_G1,
   QUE512_G2,
   QUE512_G3,
@@ -100,24 +97,27 @@ export const UPGRADING_COURSES: UpgradingCourse[] = [
   QUE513_G3,
   QUE513_G4,
   QUE513_G5,
-  // English Literature
-  QUR511,
-  // Geography
+  // QUG - Geography
   QUG511,
   QUG512,
-  // History
+  // QUH - History
   QUH512,
   QUH513,
-  // Physics
+  // QUP - Physics
   QUP511,
   QUP512,
-  // Science
+  // QUR - English Literature
+  QUR511,
+  // QUS - Science
   QUS511_G1,
   QUS511_G2,
   QUS511_G3,
   QUS511_G4,
-  // Tamil Language
+  // QUT - Tamil Language
   QUT501,
   QUT502,
   QUT503,
+  // QUY - Chemistry
+  QUY511,
+  QUY512,
 ];
